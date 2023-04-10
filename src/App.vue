6dc4d6fe48ebc164648e9265af3ed5385edf6bd2
Connor <jdzhuzhuoning@163.com>
2023-04-10 14:13:05 +0800

    2023.4.10

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld width="500px" height="500px"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
-
 export default Vue.extend({
   name: 'App',
   components: {
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// const path = require.resolve('path');
+// const util = require.resolve('util');
+const webpack = require('webpack')
+module.exports = {
+    // ...
+    // resolve: {
+    //     fallback: {
+    //         path,
+    //         util
+    //     },
+    //     alias: {
+    //         'polyfill-library': 'polyfill-library/dist/polyfill.min.js'
+    //     },
+    //     fallback: {
+    //         process: require.resolve('process/browser'),
+    //     }
+    // },
+    plugins: [
+        // new webpack.DefinePlugin({ // webpack自带该插件，无需单独安装
+        //     'process.env' : {
+        //         NODE_ENV: process.env.NODE_ENV // 将属性转化为全局变量，让代码中可以正常访问
+        //     }
+        // }),
+        new webpack.ProvidePlugin({
+            // process: require.resolve('process/browser')
+            process: 'process/browser'
+        }),
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer']
+        })
+    ]
+    // ...
+};
